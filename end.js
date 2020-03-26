830cb16a586ff6d4dee8647b38294a4a58351504
Josh Cook <cookiej91@gmail.com>
2020-03-26 15:40:32 +0000

    Add return to home after submit

--- end.js
+++ end.js
@@ -5,6 +5,8 @@ const mostRecentScore = localStorage.getItem("mostRecentScore");
 
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
+const maxHighScores = 5;
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
@@ -12,14 +14,17 @@ username.addEventListener("keyup", () => {
 });
 
 saveHighScore = e => {
+  console.log("clicked the save button!");
   e.preventDefault();
 
   const score = {
     score: mostRecentScore,
     name: username.value
   };
-
   highScores.push(score);
   highScores.sort((a, b) => b.score - a.score);
   highScores.splice(5);
+
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  window.location.assign("/");
 };

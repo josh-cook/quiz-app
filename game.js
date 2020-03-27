f3043dde0a69d58a1bba092ca12f7aebdced3be9
Josh Cook <cookiej91@gmail.com>
2020-03-27 11:46:12 +0000

    Add open trivia db api

--- game.js
+++ game.js
@@ -11,12 +11,30 @@ let questionCounter = 0;
 let availableQuestions = [];
 let questions = [];
 
-fetch("questions.json")
+fetch("https://opentdb.com/api.php?amount=10&type=multiple")
   .then(response => {
     return response.json();
   })
   .then(loadedQuestions => {
-    questions = loadedQuestions;
+    questions = loadedQuestions.results.map(loadedQuestion => {
+      const formattedQuestion = {
+        question: loadedQuestion.question
+      };
+
+      const answerChoices = [...loadedQuestion.incorrect_answers];
+      formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+      answerChoices.splice(
+        formattedQuestion.answer - 1,
+        0,
+        loadedQuestion.correct_answer
+      );
+
+      answerChoices.forEach((choice, index) => {
+        formattedQuestion["choice" + (index + 1)] = choice;
+      });
+
+      return formattedQuestion;
+    });
     startGame();
   })
   .catch(error => {
